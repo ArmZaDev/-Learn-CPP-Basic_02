b37830eb1e4042b08e53bb2abf1e79ed05575d6e
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-20 21:05:46 +0700

    Variables

--- MyProgram02/MyProgram02.cpp
+++ MyProgram02/MyProgram02.cpp
@@ -2,10 +2,14 @@
 using namespace std;
 int main()
 {
-    cout << "   /|" << endl;
-    cout << "  / |" << endl;
-    cout << " /  |" << endl;
-    cout << "/___|" << endl;
+    string chracterName = "Jhon Selby";
+    int characterAge;
+    characterAge = 25;
+
+    cout << "There once was a man named " << chracterName << endl;
+    cout << "He was " << characterAge << " years old" << endl;
+    cout << "He liked the name " << chracterName << endl;
+    cout << "But did not like being " << characterAge << endl;
        
 }
 
