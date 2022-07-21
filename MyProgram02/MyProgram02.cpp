f69b7b66785117787e6e0cee9c10de3856fec57b
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-21 16:54:14 +0700

    While loops

--- MyProgram02/MyProgram02.cpp
+++ MyProgram02/MyProgram02.cpp
@@ -1,49 +1,23 @@
 ﻿#include <iostream>
 using namespace std;
 
-string getDayOfWeek(int dayNum) 
-{
-    string dayName;
-
-    switch (dayNum) 
-    {
-        case 0:
-            dayName = "Sunday";
-            break;
-        case 1:
-            dayName = "Monday";
-            break;
-        case 2:
-            dayName = "Tuesday";
-            break;
-        case 3:
-            dayName = "Wednesday";
-            break;
-        case 4:
-            dayName = "Thursday";
-            break;
-        case 5:
-            dayName = "Friday";
-            break;
-        case 6:
-            dayName = "Sunturday";
-            break;
-        default:
-            dayName = "Invalid Day Number";
-    }
-
-    return dayName;
-}
-
 int main()
 {
-    int day;
+    int index = 1;
+    
+    while (index <= 5) {
+        cout << index << endl;
+        index++;
+    }
 
-    cout << "Enter Day (0-6): ";
-    cin >> day;
+    /*
+    index = 6;
 
-    cout << getDayOfWeek(day);
-
+    do {
+        cout << index << endl;
+        index++;
+    } while (index <= 5);
+    */
 }
 
 
