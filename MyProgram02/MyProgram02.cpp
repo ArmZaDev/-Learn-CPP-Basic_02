35771a45f2a41c4e37445fe13536d6f2c7983a38
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-21 20:07:26 +0700

    Constructor functions

--- MyProgram02/MyProgram02.cpp
+++ MyProgram02/MyProgram02.cpp
@@ -7,31 +7,20 @@ class Book
         string title;
         string author;
         int pages;
+        Book(string aTitle, string aAuthor, int aPages) {
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+        }
 };
 
 int main()
 {
     
-    Book book1;
-
-    book1.title = "Harry Potter";
-    book1.author = "JK Rowling";
-    book1.pages = 500;
-
-    Book book2;
-
-    book2.title = "Lord of the Rings";
-    book2.author = "Tolkein";
-    book2.pages = 700;
-
+    Book book1("Harry Potter", "JK Rowling", 500);
+    Book book2("Lord of the Rings", "Tolkein", 700);
+   
     cout << book1.title << endl;
-    cout << book1.author << endl;
-    cout << book1.pages << endl;
-
-    cout << book2.title << endl;
-    cout << book2.author << endl;
-    cout << book2.pages << endl;
-
 }
 
 
