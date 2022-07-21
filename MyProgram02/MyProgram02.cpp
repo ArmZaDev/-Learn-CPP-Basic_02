0115ce133e25f30cba74768c07836736b81cb967
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-21 16:19:14 +0700

    If statements (con't)

--- MyProgram02/MyProgram02.cpp
+++ MyProgram02/MyProgram02.cpp
@@ -1,17 +1,39 @@
 ﻿#include <iostream>
 using namespace std;
 
-int main()
-{
-    bool isMale = true;
-    bool isTall = false;
+int getMax(int num1, int num2, int num3) {
+    int result;
 
-    if (isMale || isTall) {
-        cout << "You are a tall male";
+    if (num1 >= num2 && num1 >= num3) { 
+        result = num1;
+    }
+    else if(num2 >= num1 && num2 >= num3) 
+    { 
+        result = num2;
     }
     else {
-        cout << "You are not male";
+        result = num3;
     }
+
+    return result;
+}
+
+int main()
+{
+    int findMax;
+    int num1, num2, num3;
+
+    cout << "Enter num1: ";
+    cin >> num1;
+    cout << "Enter num2: ";
+    cin >> num2;
+    cout << "Enter num2: ";
+    cin >> num3;
+
+    findMax = getMax(num1, num2, num3);
+    cout << findMax;
+
+    //cout << getMax(5, 10, 15);;
      
 }
 
