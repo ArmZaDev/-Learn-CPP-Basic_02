51fce03bd9d16995e82e9d4836cf5199540cf177
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-21 14:37:00 +0700

    Getting user input

--- MyProgram02/MyProgram02.cpp
+++ MyProgram02/MyProgram02.cpp
@@ -1,16 +1,22 @@
 ﻿#include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-    cout << pow(2, 5) << endl; //หาค่ายกกำลัง = 2^5
-    cout << sqrt(36) << endl; //หาค่ารูท
-    cout << round(4.1) << endl; //ปัดเศษขึ้น > 5, ปัดเศษลง < 5
-    cout << ceil(4.1) << endl; //ปัดเศษขึ้นเสมอ
-    cout << floor(4.8) << endl; //ปัดเศษลงเสมอ
-    cout << fmax(5, 10) << endl; //หาค่าสูงสุด;
-    cout << fmin(5, 10) << endl; //หาค่าต่ำสุด;
+    int age;
+
+    cout << "Enter your age: ";
+    cin >> age;
+
+    cout << "You are " << age << " years old";
+
+    /*
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+
+    cout << "Hello" << name;
+    */
      
 }
 
