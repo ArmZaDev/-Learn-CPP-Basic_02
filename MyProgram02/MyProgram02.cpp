75ad66b4fae0fabc960c3696296447b720c9ec87
ArmZaDev <nopparat.p62@rsu.ac.th>
2022-07-21 14:21:31 +0700

    Working with numbers

--- MyProgram02/MyProgram02.cpp
+++ MyProgram02/MyProgram02.cpp
@@ -1,12 +1,16 @@
-#include <iostream>
+﻿#include <iostream>
+#include <cmath>
 using namespace std;
+
 int main()
 {
-    string phrase = "Inazuma";
-
-    //cout << phrase[2] << endl;
-    //cout << phrase.find("z", 0);
-    cout << phrase.substr(0, 5);
-       
+    cout << pow(2, 5) << endl; //หาค่ายกกำลัง = 2^5
+    cout << sqrt(36) << endl; //หาค่ารูท
+    cout << round(4.1) << endl; //ปัดเศษขึ้น > 5, ปัดเศษลง < 5
+    cout << ceil(4.1) << endl; //ปัดเศษขึ้นเสมอ
+    cout << floor(4.8) << endl; //ปัดเศษลงเสมอ
+    cout << fmax(5, 10) << endl; //หาค่าสูงสุด;
+    cout << fmin(5, 10) << endl; //หาค่าต่ำสุด;
+     
 }
 
